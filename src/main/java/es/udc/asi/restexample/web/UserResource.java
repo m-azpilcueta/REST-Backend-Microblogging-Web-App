5bf214e3aa92ab8c50d19cd1d610a38be70ca6f0
martinazpilcueta <m.azpilcueta@udc.es>
2021-09-28 00:37:45 +0200

    Añadidos los endpoint activate y deactivate para los usuarios

--- src/main/java/es/udc/asi/restexample/web/UserResource.java
+++ src/main/java/es/udc/asi/restexample/web/UserResource.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.udc.asi.restexample.model.exception.NotFoundException;
+import es.udc.asi.restexample.model.exception.OperationNotAllowed;
 import es.udc.asi.restexample.model.service.UserService;
 import es.udc.asi.restexample.model.service.dto.UserDTOPublic;
 
@@ -27,5 +30,15 @@ public class UserResource {
   @DeleteMapping("/{id}")
   public void delete(@PathVariable Long id) {
     userService.deleteById(id);
+  }
+  
+  @PutMapping("/{id}/activate")
+  public void activate(@PathVariable Long id) throws NotFoundException, OperationNotAllowed {
+	  userService.updateActive(id, true);
+  }
+  
+  @PutMapping("/{id}/deactivate")
+  public void deactivate(@PathVariable Long id) throws NotFoundException, OperationNotAllowed {
+	  userService.updateActive(id, false);
   }
 }
