253f84101ea093ba8ebf4b5f33cdfdbd03f65294
martinazpilcueta <m.azpilcueta@udc.es>
2021-09-27 19:50:23 +0200

    Implementado el borrado de posts con comprobacion de usuario y tiempo desde creacion

---
+++ src/main/java/es/udc/asi/restexample/model/exception/OperationNotAllowed.java
@@ -0,0 +1,9 @@
+package es.udc.asi.restexample.model.exception;
+
+public class OperationNotAllowed extends ModelException {
+
+	public OperationNotAllowed(String msg) {
+		super("Operation not allowed: " + msg);
+	}
+
+}
