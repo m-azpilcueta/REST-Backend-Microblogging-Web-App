77891b89db7c63229db2c506c4be77dcae86d77f
martinazpilcueta <m.azpilcueta@udc.es>
2021-09-28 00:45:21 +0200

    Añadidas las fechas de cracion de post a PostDTO

--- src/main/java/es/udc/asi/restexample/model/service/dto/PostDTO.java
+++ src/main/java/es/udc/asi/restexample/model/service/dto/PostDTO.java
@@ -1,5 +1,6 @@
 package es.udc.asi.restexample.model.service.dto;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -11,66 +12,76 @@ import com.sun.istack.NotNull;
 import es.udc.asi.restexample.model.domain.Post;
 
 public class PostDTO {
-  private Long id;
-  @NotEmpty
-  private String title;
-  @NotEmpty
-  private String body;
-  @NotNull
-  private UserDTOPublic author;
-  private List<TagDTO> tags = new ArrayList<>();
+	private Long id;
+	@NotEmpty
+	private String title;
+	@NotEmpty
+	private String body;
+	@NotNull
+	private UserDTOPublic author;
+	private List<TagDTO> tags = new ArrayList<>();
+	private LocalDateTime timestamp;
 
-  public PostDTO() {
-  }
+	public PostDTO() {
+	}
 
-  public PostDTO(Post post) {
-    this.id = post.getId();
-    this.title = post.getTitle();
-    this.body = post.getBody();
-    this.author = new UserDTOPublic(post.getAuthor());
-    post.getTags().forEach(t -> {
-      this.tags.add(new TagDTO(t));
-    });
-    this.tags.sort(Comparator.comparing(TagDTO::getName));
-  }
+	public PostDTO(Post post) {
+		this.id = post.getId();
+		this.title = post.getTitle();
+		this.body = post.getBody();
+		this.author = new UserDTOPublic(post.getAuthor());
+		post.getTags().forEach(t -> {
+			this.tags.add(new TagDTO(t));
+		});
+		this.tags.sort(Comparator.comparing(TagDTO::getName));
+		this.timestamp = post.getTimestamp();
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public Long getId() {
+		return id;
+	}
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-  public String getTitle() {
-    return title;
-  }
+	public String getTitle() {
+		return title;
+	}
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-  public String getBody() {
-    return body;
-  }
+	public String getBody() {
+		return body;
+	}
 
-  public void setBody(String body) {
-    this.body = body;
-  }
+	public void setBody(String body) {
+		this.body = body;
+	}
 
-  public UserDTOPublic getAuthor() {
-    return author;
-  }
+	public UserDTOPublic getAuthor() {
+		return author;
+	}
 
-  public void setAuthor(UserDTOPublic author) {
-    this.author = author;
-  }
+	public void setAuthor(UserDTOPublic author) {
+		this.author = author;
+	}
 
-  public List<TagDTO> getTags() {
-    return tags;
-  }
+	public List<TagDTO> getTags() {
+		return tags;
+	}
 
-  public void setTags(List<TagDTO> tags) {
-    this.tags = tags;
-  }
+	public void setTags(List<TagDTO> tags) {
+		this.tags = tags;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
 }
